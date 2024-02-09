e7c9c1fed4c1df353509935ead86cb5d754861be
tan.lehuu <tan.lehuu@lazada.com>
2024-02-09 23:46:50 +0700

    Init spring 6 study web app

---
+++ src/main/java/com/tanlh/spring6app/Spring6AppApplication.java
@@ -0,0 +1,13 @@
+package com.tanlh.spring6app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanlh/spring6app/Spring6AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanlh.spring6app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
